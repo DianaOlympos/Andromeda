3e4d3a23aea8dd3f8405ef8b00548e7d39d5ebb9
Depierre Thomas <depierre.thomas@gmail.com>
2016-02-17 00:30:55 +0100

    Adds deps for SSO

--- lib/CrestSSO/guardian_serializer.ex
+++ lib/CrestSSO/guardian_serializer.ex
@@ -4,7 +4,7 @@ defmodule Evesurvey.GuardianSerializer do
   alias Evesurvey.Repo
   alias Evesurvey.User
 
-  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
+  def for_token(user = %{id: _}), do: { :ok, "User:#{user.id}" }
   def for_token(_), do: { :error, "Unknown resource type" }
 
   def from_token("User:" <> id), do: { :ok, Repo.get(User, String.to_integer(id))}
