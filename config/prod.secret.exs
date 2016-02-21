251a3459d5ea584a2625152c7d39c82597437830
Depierre Thomas <depierre.thomas@gmail.com>
2016-02-21 17:51:25 +0100

    Do not do that at home. Pushing secret to git

---
+++ config/prod.secret.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :andromeda, Andromeda.Endpoint,
+  secret_key_base: "gf8P2S8sxoDj2hADfGbjM3bTc2aSWOJEB3mf6qxeoWFgnF8zWeEZMd4pDV7F6jTk"
+
+# Configure your database
+config :andromeda, Andromeda.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "andromeda_prod",
+  pool_size: 20
+
+config :andromeda,
+  client_id: "0ae0d33077f64f5684d4b4f98aa9b103",
+  client_secret: "5POw9ETaNqm7ZgdgNEJILGScR0pIUbHXL1AyVFf1",
+  redirect_uri: "http://localhost:4000/auth/callback"
