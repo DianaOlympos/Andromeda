0489770c44863989a5cf8c392eb3d88676a67830
Depierre Thomas <depierre.thomas@gmail.com>
2016-02-28 20:30:50 +0100

    Adds scope LocationWrite on prod.secret

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -18,4 +18,4 @@ config :andromeda,
   client_id: "0ae0d33077f64f5684d4b4f98aa9b103",
   client_secret: "5POw9ETaNqm7ZgdgNEJILGScR0pIUbHXL1AyVFf1",
   redirect_uri: "http://localhost:4000/auth/callback",
-  scope: "characterLocationRead"
+  scope: "characterLocationRead characterNavigationWrite"
