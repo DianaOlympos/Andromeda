63cefebe2f653891f04ea9e5ee2b9fee4cf21f34
Thomas Depierre <depierre.thomas@gmail.com>
2016-03-02 00:11:37 +0100

    Correct page.view

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,8 +1,11 @@
 defmodule Andromeda.PageView do
   use Andromeda.Web, :view
 
-  def is_authed?(user) do
-    user != nil
+  def is_authed?(id) do
+    case EveUser.UserDetails.get_user(id) do
+      user -> true
+      {:error, _msg) -> false
+    end
   end
 
 end
