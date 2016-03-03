8f153c5bf30d802e288d3182bb50304817b89035
Depierre Thomas <depierre.thomas@gmail.com>
2016-03-03 22:15:12 +0100

    id AND name

--- web/channels/fleets_channel.ex
+++ web/channels/fleets_channel.ex
@@ -4,7 +4,7 @@ defmodule Andromeda.FleetsChannel do
   def join("pilot:" <> _user_id , _message, socket) do
     user = Guardian.Phoenix.Socket.current_resource(socket)
     fleet = EveFleet.Fleet.get_data_id(user.fleet)
-    members=Enum.map(fleet.members_list,fn x -> EveUser.User.get_user_name(x) end)
+    members=Enum.map(fleet.members_list,fn x -> %{:id => x, :name => EveUser.User.get_user_name(x)} end)
     {:ok, members, socket}
   end
 
