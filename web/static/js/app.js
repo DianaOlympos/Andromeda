a03481fdfcbcc450e6e4e0da0453078a4fd16893
Robert Schuh <github@eneticum.de>
2016-03-05 00:40:35 +0100

    strane render stuff + follow fc toggle

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -24,18 +24,163 @@
 	
 	import {Socket} from "phoenix";
 
-	let guardianToken = $('meta[name="guardian_token"]').getAttribute('content');
-	let socket = new Socket("/socket", { params: { guardian_token: guardianToken } });
+	const canvas = $("#map");
+	const ctx = canvas.getContext("2d");
+
+	window.addEventListener("resize", drawMap);
+
+
+	const andi = {
+		map: [],
+		me: null,
+		pilotlist: []
+	};
+
+	$("#followfc").addEventListener("change", () => andi.pilotChannel.push("follow_fc", $("#followfc").checked));
+
+	const guardianToken = $('meta[name="guardian_token"]').getAttribute('content');
+	const socket = new Socket("/socket", { params: { guardian_token: guardianToken } });
 	socket.connect();
 
-	let pilotChannel;
-	let fleetChannel = socket.channel("fleet:" + window.location.pathname.split("/").splice(-1, 1)[0], {});
-	fleetChannel.join()
+	andi.fleetChannel = socket.channel("fleet:" + window.location.pathname.split("/").splice(-1, 1)[0], {});
+	
+	andi.fleetChannel
+		.on("pilot_list", resp => { andi.pilotlist = resp; drawPilotlist(); });
+	
+	andi.fleetChannel.join()
 		.receive("ok", resp => { 
+
 			console.log("Joined fleet successfully", resp); 
-			pilotChannel = socket.channel("pilot:" + resp);
-			pilotChannel.join()
+
+			andi.pilotChannel = socket.channel("pilot:" + resp);
+			
+			andi.pilotChannel.on("location_member", resp => {});
+			andi.pilotChannel.on("location", resp => { andi.me = resp; drawMap(); });
+			andi.pilotChannel.on("map", resp => { andi.map = resp.map; drawMap(); });
+
+			andi.pilotChannel.join()
 				.receive("ok", resp => { console.log("Joined pilot successfully", resp); })
 				.receive("error", resp => { console.log("Unable to join", resp); });
+
 		})
 		.receive("error", resp => { console.log("Unable to join", resp); });
+
+
+	function drawMap () {
+		console.log("draw map");
+		if(andi.map.length === 0 || !andi.me)
+			return;
+
+		let width = $("main").clientWidth - 20;
+		let height = $("main").clientHeight - 20;
+
+		let scale = Math.min(width, height);
+		let center = [width / 2, height / 2];
+
+		let ringdist = scale / 2 / 6;
+
+		canvas.width = width;
+		canvas.height = height;
+
+		let depths = [
+			[],
+			[],
+			[],
+			[],
+			[],
+			[]
+		];
+
+		andi.map.map(system => depths[system.depth].push(system));
+
+		drawSystem(center, depths[0][0]);
+
+		let sysdist = 360 / depths[1].length;
+
+		console.log(depths[1], ringdist, sysdist);
+
+
+
+	
+		depths.map(depth => 
+			depth
+				.map(system => !(system.render = {}) || system)
+				//.map((system, ind) => !(system.render.degrees = ind * (360 / depth.length)) || system)
+				.map((system, ind) => {
+
+					let parent = getParent(system);
+					let splitDeg = 360 / depth.length;
+					let shards = filterByDepth(parent.connection.map(getSystemByID), system.depth);
+					let shardIDs = shards.map(shard => shard.id);
+					let shardDeg = splitDeg / (shards.length == depth.length ? 1 : shardIDs.length);
+
+					system.render.degrees = (parent.render.degrees) - (splitDeg / 2) + (shardDeg * shardIDs.indexOf(system.id));
+
+					return system;
+				})
+		);
+
+
+
+	
+		console.log("depths");
+		console.log(depths);
+
+		
+		andi.map
+			.map(system => 
+				system.connection.map(systemID => connectSystems(center, ringdist, system, getSystemByID(systemID))) && (drawSystem([
+					center[0] - ((ringdist * system.depth) * Math.cos(toRadians(system.render.degrees))),
+					center[1] - ((ringdist * system.depth) * Math.sin(toRadians(system.render.degrees)))
+				], system))
+			);
+
+
+	}
+
+	function drawSystem (cors, system) {
+		ctx.beginPath();
+		ctx.arc(cors[0], cors[1], 20, 0, Math.PI * 2);
+		ctx.fill();
+	}
+
+	function connectSystems (center, ringdist, systemA, systemB) {
+		if(!systemA || !systemB)
+			return;
+		ctx.strokeStyle = "black";
+		ctx.beginPath();
+		ctx.moveTo(
+			center[0] - ((ringdist * systemA.depth) * Math.cos(toRadians(systemA.render.degrees))),
+			center[1] - ((ringdist * systemA.depth) * Math.sin(toRadians(systemA.render.degrees)))
+		);
+		ctx.lineTo(
+			center[0] - ((ringdist * systemB.depth) * Math.cos(toRadians(systemB.render.degrees))),
+			center[1] - ((ringdist * systemB.depth) * Math.sin(toRadians(systemB.render.degrees)))
+		);
+		ctx.stroke();
+	}
+
+	function drawPilotlist () {
+
+	}
+
+	function toDegrees (angle) {
+		return angle * (180 / Math.PI);
+	}
+
+	function toRadians (angle) {
+		return angle * (Math.PI / 180);
+	}
+
+	function getSystemByID (systemID) {
+		return andi.map.find(system => system.id == systemID);
+	}
+
+	function filterByDepth(systems, depth) {
+		return systems.filter(system => system.depth == depth);
+	}
+
+	function getParent (system) {
+		console.log(system, filterByDepth(andi.map, system.depth - 1));
+		return filterByDepth(andi.map, system.depth - 1).find(sys => sys.connection.indexOf(system.id) != -1) || { render: { degrees: 0 }, connection: [system.id] };
+	}
