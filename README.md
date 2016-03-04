7487c2cc51172f4d1151ff73d6d9791c39b89ba8
Thomas Depierre <depierre.thomas@gmail.com>
2016-03-05 00:58:24 +0100

    ready for release

---
+++ README.md
@@ -0,0 +1,19 @@
+A little fleet helper
+
+
+tons of issues, map a bit useless
+
+# To install
+install erlang 18 +(https://www.erlang-solutions.com/resources/download.html)
+install elixir 1.2+ (same url)
+
+git clone
+cd Andromeda
+
+in config/prod.secret.exs fill your application callback and secret
+
+in cli
+mix phoenix_server
+follow the instruction to get hex and rebar.
+
+Only work on local for now.
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -7,8 +7,8 @@ config :andromeda, Andromeda.Endpoint,
   secret_key_base: "gf8P2S8sxoDj2hADfGbjM3bTc2aSWOJEB3mf6qxeoWFgnF8zWeEZMd4pDV7F6jTk"
 
 config :andromeda,
-  client_id: "0ae0d33077f64f5684d4b4f98aa9b103",
-  client_secret: "5POw9ETaNqm7ZgdgNEJILGScR0pIUbHXL1AyVFf1",
-  redirect_uri: "http://localhost:4000/auth/callback",
+  client_id: "",
+  client_secret: "",
+  redirect_uri: "",
   scope: "characterLocationRead characterNavigationWrite",
-  user_agent: "Andromeda Thomas Depierre"
+  user_agent: ""
